5638c44924db871f4ae1ed757d5b7fa09f2bb3ea
Josiah Ayres <josiahayres@gmail.com>
2016-08-25 02:34:26 +0000

    events and unused php code removed

--- form.php
+++
@@ -1,9 +0,0 @@
-<?php
-include('db_login.php');
-include('db.php');
-
-$lat = isset($_POST['lat']) ? $_POST['lat'] : null;
-$lng = isset($_POST['lng']) ? $_POST['lng'] : null;
-
-echo '<script>myfunction()</script>';
-?>
